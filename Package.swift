a78a2683bbf33ae6157d7b97e103d66604136298
Alfian Losari <alfianlosari@gmail.com>
2024-05-20 14:27:21 +0700

    Update Highlightr Swift dep version

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/alfianlosari/ChatGPTSwift.git", from: "2.2.0"),
         .package(url: "https://github.com/apple/swift-markdown.git", revision: "swift-5.10-RELEASE"),
-        .package(url: "https://github.com/alfianlosari/HighlighterSwift.git", branch: "main")
+        .package(url: "https://github.com/alfianlosari/HighlighterSwift.git", from: "1.0.0")
     ],
     targets: [
         .target(
